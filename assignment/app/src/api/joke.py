b25691d0d343762e4ba784979c9406e9a45d023e
timuraiz <aiztim02@gmail.com>
2023-11-05 20:40:25 +0300

    ml part binded to back

--- assignment/app/src/api/joke.py
+++ assignment/app/src/api/joke.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from assignment.model.joke_generation import generate_joke
+
 templates = Jinja2Templates(directory="/code/src/templates")
 router = APIRouter()
 
@@ -9,7 +11,7 @@ router = APIRouter()
 @router.post("/new_joke", response_class=HTMLResponse)
 def complete_joke(request: Request, start: str = Form(...)):
     try:
-        full_joke = "This is where the completed joke would go."
+        full_joke = generate_joke(start)
         return templates.TemplateResponse("index.html", {"request": request, "joke": full_joke})
     except Exception as e:
         return HTMLResponse(content=f"<p>Error: {str(e)}</p>", status_code=500)
