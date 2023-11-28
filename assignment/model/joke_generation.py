6d81fee10e946b64c0241a6d516d3d0169ece020
timuraiz <aiztim02@gmail.com>
2023-11-28 21:05:57 +0300

    test

--- assignment/model/joke_generation.py
+++ assignment/model/joke_generation.py
@@ -1,27 +1,52 @@
 import tiktoken
 import torch
 
-from .gpt import Config, GPT, device
+from .gpt import Config, GPT
+
 from assignment import ROOT_DIR
 
-load_model_path = f'{ROOT_DIR}/model/model'  # here is the path to saved model
+model_1, model_2 = None, None
+enc = None
 
-enc = tiktoken.get_encoding("gpt2")
+models = {
+    'gpt': model_1,
+    'gpt2': model_2,
+}
 
-max_seq_len = 100
-tokens = 50257
+device = 'cpu'
 
 
-def generate_joke(sentences):
+def load_gpt_model():
+    global model_1, enc
+    load_model_path = f'{ROOT_DIR}/assignment/model/model_1'
+    enc = tiktoken.get_encoding("gpt2")
+    tokens = 50257
+    max_seq_len = 50
+
+    config = Config(enc.n_vocab, max_seq_len)
+    model_1 = GPT(config).to(device)
+    model_1.load_state_dict(torch.load(load_model_path, map_location=torch.device('cpu')))
+    model_1.eval()
+
+
+def load_gpt2_model():
+    global model_2
+    load_model_path = f'{ROOT_DIR}/assignment/model_1'
+    # TODO: write script that loads second model
+
+
+def generate_joke(starting_string, model_choice):
     '''
     Something smart happens here.
     :param sentences:
     :return:
     '''
-    config = Config(tokens, max_seq_len)
-    model = GPT(config).to(device)
-    model.load_state_dict(torch.load(load_model_path, map_location=torch.device('cpu')))
-    model.eval()
+    print('hERE')
+    print(model_choice)
+    print(starting_string, type(starting_string))
 
-    start = torch.tensor([enc.encode(sentences)], dtype=torch.long, device=device)
-    return enc.decode(model.generate(xs=start, max_new_tokens=30, do_sample=True)[0].tolist())
+    start = torch.tensor([enc.encode(starting_string)], dtype=torch.long, device=device)
+
+    output = model_choice[model_choice].generate(xs=start, max_new_tokens=30, do_sample=True)[0].tolist()
+    output = output[:output.index(enc.eot_token)]
+    return enc.decode(output)
